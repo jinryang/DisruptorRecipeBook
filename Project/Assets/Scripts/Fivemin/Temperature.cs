67aa9d9ca1a7f757df5d5bce2df9ec5c8e931024
ssy05 <87408309+ssy05@users.noreply.github.com>
2022-08-05 19:56:51 +0900

    Fivemin

---
+++ Project/Assets/Scripts/Fivemin/Temperature.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temperature : MonoBehaviour
+{
+    public Slider TemperatureSlider;
+    public int Maxtem = 200;
+    public int Mintem = 30;
+    public float nowtem;
+    public float DectemSpeed = 0.3f;
+    private bool isClick;
+    public void ButtonDown()
+    {
+        isClick = true;
+        Debug.Log("asdf");
+    }
+    public void ButtonUp()
+    {
+        isClick = false;
+        Debug.Log("fdsa");
+    }
+    void Start()
+    {
+        nowtem = Mintem;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        TemperatureSlider.value = nowtem;
+        if (isClick == true)
+        {
+            nowtem += DectemSpeed;
+
+            if (nowtem >= Maxtem)
+                nowtem = Maxtem;
+        }
+        else
+        {
+            nowtem -= DectemSpeed - 0.1f;
+
+            if (nowtem <= Mintem)
+                nowtem = Mintem;
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Fivemin/Timer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    int a = 0;
+    public bool reset;
+    public bool stop;
+    public float time = 20;
+    public GameObject tembutton;
+    public TextMeshProUGUI TimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reset = false;
+        stop = false;
+        time = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop == false)
+        {
+            if (time > 0)
+                time -= Time.deltaTime;
+            else
+            {
+                time = 0;
+                tembutton.SetActive(false);
+                if (a == 0)
+                {
+                    Invoke("TimeReset", 3.0f);
+                    a++;
+                }
+            }
+        }
+        TimeText.text = string.Format("{0:N2}", time);
+    }
+    public void OutButton()
+    {
+        stop = true;
+    }
+    public void TimeReset()
+    {
+        time = 20;
+    }
+}
