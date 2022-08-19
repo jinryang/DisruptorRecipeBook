0aa2635c74a879437a0fab548e950bcbd8baad38
ssy05 <87408309+ssy05@users.noreply.github.com>
2022-08-20 02:47:32 +0900

    a

--- Project/Assets/Scripts/Oven/Point.cs
+++ Project/Assets/Scripts/Oven/Point.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Point : MonoBehaviour
 {
     private int point;
+    public TextMeshProUGUI pointtext;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,10 @@ public class Point : MonoBehaviour
     public void AddPoint(int add)
     {
         point += add;
+    }
+    void FixedUpdate()
+    {
+        pointtext.text = "POINT : " + point;
+        pointtext.color = Color.red;
     }
 }
