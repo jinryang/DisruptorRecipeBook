90fad4ae25bf99f0ad8e57d86debc449317415fc
ssy05 <87408309+ssy05@users.noreply.github.com>
2022-08-23 19:45:46 +0900

    a

--- Project/Assets/Scripts/Stack/Move.cs
+++ Project/Assets/Scripts/Stack/Move.cs
@@ -9,14 +9,22 @@ public class Move : MonoBehaviour
     public float delta = 10.0f;
     public float speed = 1.0f;
     public int stop = 0;
+    public GameObject drop;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         stop = 0;
         pos = transform.position;
+        rigid.gravityScale = 0;
     }
     void FixedUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            stop = 1;
+            Instantiate(drop);
+        }
         Vector3 v = pos;
         if(stop == 0)
         {
