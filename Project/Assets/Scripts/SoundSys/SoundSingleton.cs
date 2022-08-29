3af055aa4a3f4dc282b5f5233137467af9f29803
ksm1028 <84126337+ksm1028@users.noreply.github.com>
2022-08-29 19:29:31 +0900

    SoundManager

---
+++ Project/Assets/Scripts/SoundSys/SoundSingleton.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSingleton : MonoBehaviour
+{
+    [SerializeField] AudioClip[] BGM;
+    [SerializeField] AudioClip[] EF;
+    AudioSource audio;
+    public float fadeRange;
+
+    private WaitForSeconds wait = new WaitForSeconds(0.01f);    
+
+    public SoundSingleton soundSingleton = null;
+    private void Awake()
+    {
+        if(soundSingleton == null)
+        {
+            soundSingleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(soundSingleton != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(bool IsBGM, int numof)
+    {
+        if(IsBGM == true)
+        {
+            audio.clip = BGM[numof - 1];
+            audio.Play();
+        }
+        else
+        {
+            audio.clip = EF[numof - 1];
+            audio.Play();
+        }
+    }
+
+    public void stop()
+    {
+        audio.Stop();
+    }
+
+    public void FadeInBGM()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeOutBGM()
+    {
+        StopAllCoroutines(); 
+        StartCoroutine(FadeOut());
+    }
+    IEnumerator FadeIn()
+    {
+        for (float i = 0f; i <= 1f; i += fadeRange)
+        {
+            audio.volume = i;
+            yield return wait;
+        }
+    }
+    IEnumerator FadeOut()
+    {
+        for (float i = 1.0f; i >= 0f; i -= fadeRange)
+        {
+            audio.volume = i;
+            yield return wait;
+        }
+    }
+}
