fa834a66563693a2fd9d3f4a94047f88acdb53d6
jinryang <84126292+jinryang@users.noreply.github.com>
2022-09-02 20:10:39 +0900

    Tutorial, result scene
    make

---
+++ Project/Assets/Scripts/MiniGame/ScoreCalculation/ResultScore.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScore : MonoBehaviour
+{
+    [SerializeField] private Text t1;
+    [SerializeField] private Text t2;
+    [SerializeField] private Text t3;
+    [SerializeField] private Text result;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
