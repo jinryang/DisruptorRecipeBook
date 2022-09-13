5980619969e956d888754f68457c7555e31f2834
jinryang <84126292+jinryang@users.noreply.github.com>
2022-09-13 20:52:44 +0900

    LoadScenes

---
+++ Project/Assets/Scripts/System/MinigameManagement.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinigameManagement : MonoBehaviour
+{
+    #region Singleton
+    private static MinigameManagement instance;
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static MinigameManagement Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                instance = null;
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public int[] moveScenes;
+    public int[] points;
+    public int idx = 0;
+
+    public void SelectRecipe(int number)//게임 시작 버튼이 눌리면 레시피의 번호가 나온다.
+    {
+        moveScenes = RecipeDatas.Instance.Recipes[number].moveScene;
+        points = new int[moveScenes.Length];
+        idx = 0;
+    }
+
+    public void GoTutorial()
+    {
+        switch (moveScenes[idx])
+        {
+            case 1:
+            case 2:
+            case 3:
+                {
+                    SceneManager.LoadScene("GubGiPenHow");
+                    break;
+                }
+            case 4:
+                {
+                    SceneManager.LoadScene("OvenSceneHow");
+                    break;
+                }
+            case 5:
+            case 6:
+                {
+                    SceneManager.LoadScene("BoilHow");
+                    break;
+                }
+            case 7:
+            case 8:
+            case 9:
+                {
+                    SceneManager.LoadScene("SprinkleHow");
+                    break;
+                }
+            case 10:
+                {
+                    SceneManager.LoadScene("CutSceneHow");
+                    break;
+                }
+            case 11:
+            case 12:
+                {
+                    SceneManager.LoadScene("OilFriHow");
+                    break;
+                }
+            case 13:
+                {
+                    SceneManager.LoadScene("FriHow");
+                    break;
+                }
+            case 14:
+                {
+                    SceneManager.LoadScene("StackHow");
+                    break;
+                }
+            case 15:
+            case 16:
+                {
+                    Debug.Log("아직 존재하지 않는다.");
+                    break;
+                }
+        }
+    }
+    public void GoGame()
+    {
+        switch (moveScenes[idx++])//로드할 씬 집어넣기
+        {
+            case 1: { SceneManager.LoadScene("GubGiPenB"); break; }
+            case 2: { SceneManager.LoadScene("GubGiPenN"); break; }
+            case 3: { SceneManager.LoadScene("GubGiPenS"); break; }
+            case 4: { SceneManager.LoadScene("OvenB"); break; }
+            case 5: { SceneManager.LoadScene("BoilG"); break; }
+            case 6: { SceneManager.LoadScene("BoilT"); break; }
+            case 7: { SceneManager.LoadScene("SprinkleBS"); break; }
+            case 8: { SceneManager.LoadScene("SprinkleFL"); break; }
+            case 9: { SceneManager.LoadScene("SprinkleBL"); break; }
+            case 10: { SceneManager.LoadScene("CutN"); break; }
+            case 11: { SceneManager.LoadScene("OilFriN"); break; }
+            case 12: { SceneManager.LoadScene("OilFriF"); break; }
+            case 13: { SceneManager.LoadScene("FriG"); break; }
+            case 14: { SceneManager.LoadScene("Stack"); break; }
+            case 15: { Debug.Log("아직 존재하지 않는다."); break; }
+            case 16: { Debug.Log("아직 존재하지 않는다."); break; }
+        }
+    }
+}
