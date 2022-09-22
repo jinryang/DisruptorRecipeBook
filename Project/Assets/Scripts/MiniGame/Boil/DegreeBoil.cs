d8b80af0953ff3da1f2c923f0d4a8eb848b7d23d
jinryang <84126292+jinryang@users.noreply.github.com>
2022-09-22 19:10:32 +0900

    a
    a

--- Project/Assets/Scripts/MiniGame/Boil/DegreeBoil.cs
+++ Project/Assets/Scripts/MiniGame/Boil/DegreeBoil.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class DegreeBoil : MonoBehaviour
 {
     private float degree = 0;
-
+    public Sprite[] sp;
+    int point;
     void FixedUpdate()
     {
+        if (degree < 3)
+        {
+            GetComponent<SpriteRenderer>().sprite = sp[0];
+            point = 0;
+        }
+        else if (degree < 6)
+        {
+            GetComponent<SpriteRenderer>().sprite = sp[1];
+            point = 30;
+        }
+        else if (degree < 9)
+        {
+            GetComponent<SpriteRenderer>().sprite = sp[2];
+            point = 50;
+        }
+        else if (degree < 12)
+        {
+            GetComponent<SpriteRenderer>().sprite = sp[3];
+            point = 70;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = sp[4];
+            point = 10;
+        }
+
         if (GetComponent<ObjectDragBoil>().onAble && !(GetComponent<ObjectDragBoil>().isMoving))
         {
             degree += Time.deltaTime;
@@ -16,12 +43,6 @@ public class DegreeBoil : MonoBehaviour
 
     public int Point()
     {
-        int point;
-        if (degree < 3) { point = 0; }
-        else if (degree < 6) { point = 30; }
-        else if (degree < 9) { point = 50; }
-        else if (degree < 12) { point = 70; }
-        else { point = 10; }
         return point;
     }
 }
